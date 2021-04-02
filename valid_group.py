4390ab7396af23f6bd4627737634005768fd0e31
Henke <kultala@kth.se>
2021-04-02 13:31:46 +0200

    Adds a depth option for the folder examination

--- valid_group.py
+++ valid_group.py
@@ -24,10 +24,33 @@ def subgroups_recursion(group_members: "list of str", index : int, member_inclus
 # Returns a lost of tuples with the number of collaborations among the subgroups of the group members, along with which group members are part of this.
 # The subgroups considered are at least of size 2.
 # The special case of a group with a single members does not count as a subset to anything else but itself.
-def most_collaborations(base_folder : str, group_members : "list of str") -> "list of pairs (int, list of str)":
+def most_collaborations(base_folder : str, group_members : "list of str", depth : int) -> "list of pairs (int, list of str)":
     # https://stackoverflow.com/questions/973473/getting-a-list-of-all-subdirectories-in-the-current-directory 
-    folder_lists = [x[1] for x in os.walk(base_folder) if x[1] != []]
-    folders = [item for sublist in folder_lists for item in sublist]
+    if depth < 0:
+        folder_lists = [x[1] for x in os.walk(base_folder) if x[1] != []]
+        folders = [item for sublist in folder_lists for item in sublist]
+    else:
+        current_depth = [base_folder]
+        for i in range(depth + 1):
+            next_depth = []
+            for f in current_depth:
+                nxt = []
+                # This loop is needed since python seemingly has no has_next function, meaning next(os.walk(f)) might throw an exception.
+                for n in os.walk(f):
+                    nxt = n
+                    break
+                # We prepend the path so that we can use the folder paths in os.walk, 
+                # unless we are at the last iterations in which case we only want the folder names.
+                if nxt == []:
+                    continue
+                if i == depth:
+                    next_depth += [x for x in nxt[1] if x != []]
+                else:
+                    path = nxt[0]
+                    path = path + "/" if path[-1] != "/" else path
+                    next_depth += [path + x for x in nxt[1] if x != []]
+            current_depth = next_depth
+        folders = current_depth
     
     subgroupsR = subgroups_recursion(group_members, 0)
     subgroups = [sub for sub in subgroupsR if len(sub) > 1]
@@ -64,14 +87,18 @@ def most_collaborations(base_folder : str, group_members : "list of str") -> "li
 # - a string with the path to the base folder, 
 # - a string with the group folder name,
 # - an int with the maximum group size,
-# - and an int with the maximum number of times the same group is allowed to work together (this includes groups of only a single person).
+# - an int with the maximum number of times the same group is allowed to work together (this includes groups of only a single person).
+# - and an int specifying what depth from the base folder should be examined. A depth of 0 means only the folder names of the direct children of the base folder
+# are examined (and not any descendents to them), while a depth of 1 means only the children of the children of the base folder are examined. A depth of -1
+# means all descendent folder of the base folder will be examined.
 def main() -> "no return":
     report = ""
     valid_group = True
     verdict = ""
 
     group_members = extract_and_sort_names(sys.argv[2])
-    collaborations = most_collaborations(sys.argv[1], group_members)
+    depth = int(sys.argv[5])
+    collaborations = most_collaborations(sys.argv[1], group_members, depth)
     print(collaborations, "\n\n")
     allowed_group_size = int(sys.argv[3])
     allowed_collaboration_times = int(sys.argv[4])
