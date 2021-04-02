83b1d220f435bee016da178da9c9911ae712ece7
EleonoraBorzis <47255376+EleonoraBorzis@users.noreply.github.com>
2021-04-02 11:50:37 +0200

    Update getJson.py

--- getJson.py
+++ getJson.py
@@ -7,6 +7,8 @@ def get_values_json(payload):
   #  payloads = json.dumps(payload)
     quotes_payload = json.loads(payload)
     main_branch = quotes_payload['pull_request']['base']['ref']
+    print("Ok")
+    print(main_branch)
     head_branch = quotes_payload['pull_request']['head']['ref']
     main_repo = quotes_payload['pull_request']['base']['repo']['full_name']
     head_repo = quotes_payload['pull_request']['head']['repo']['full_name']
@@ -21,5 +23,4 @@ if __name__ == "__main__":
     with open(path, 'r') as myfile:
       data=myfile.read()
     print(data)
-    print("Ok")
     get_values_json(data)
