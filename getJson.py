bc47fd126961bac8ce2b8a7039b262f430bcba54
EleonoraBorzis <47255376+EleonoraBorzis@users.noreply.github.com>
2021-04-02 11:33:26 +0200

    Update getJson.py

--- getJson.py
+++ getJson.py
@@ -6,10 +6,10 @@ import json
 def get_values_json(payload):
    # payloads = json.dumps(payload)
     quotes_payload = json.loads(payload)
-    main_branch = quotes_payload["pull_request"]["base"]["ref"]
-    head_branch = quotes_payload["pull_request"]["head"]["ref"]
-    main_repo = quotes_payload["pull_request"]["base"]["repo"]["full_name"]
-    head_repo = quotes_payload["pull_request"]["head"]["repo"]["full_name"]
+    main_branch = quotes_payload['pull_request']['base']['ref']
+    head_branch = quotes_payload['pull_request']['head']['ref']
+    main_repo = quotes_payload['pull_request']['base']['repo']['full_name']
+    head_repo = quotes_payload['pull_request']['head']['repo']['full_name']
      
     print("::set-output name=baseBr::" + main_branch)
     print("::set-output name=headBr::" + head_branch)
