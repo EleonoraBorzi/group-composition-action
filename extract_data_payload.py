418f9377c0597b1c04b3323f80d456f13d8f3de9
EleonoraBorzis <47255376+EleonoraBorzis@users.noreply.github.com>
2021-04-04 16:55:48 +0200

    Update extract_data_payload.py

--- extract_data_payload.py
+++ extract_data_payload.py
@@ -6,20 +6,25 @@ import json
 def get_values_json(payload):
     payloads = json.dumps(payload)
     quotes_payload = json.loads(payload)
+    #Extract branch of main repo
     main_branch = quotes_payload['pull_request']
     main_branch = main_branch['base']
     main_branch = main_branch['ref']
+    #Extract branch of forked repo
     head_branch = quotes_payload['pull_request']
     head_branch = head_branch['head']
     head_branch = head_branch['ref']
+    #Extract name of main repo
     main_repo = quotes_payload['pull_request']
     main_repo = main_repo['base']
     main_repo = main_repo['repo']
     main_repo = main_repo['full_name']
+    #Extract name of forked repo
     head_repo = quotes_payload['pull_request']
     head_repo = head_repo['head']
     head_repo = head_repo['repo']
     head_repo = head_repo['full_name']
+    #Extract pull request number 
     pull_number = quotes_payload['number']
     
 
@@ -28,13 +33,7 @@ def get_values_json(payload):
     print("::set-output name=baseRepo::" + main_repo)
     print("::set-output name=headRepo::" + head_repo)
     print("::set-output name=pullNumber::" + str(pull_number)) 
-    
-    
-   # print("::set-output name=baseBr::" + main_branch)
-   # print("::set-output name=headBr::" + head_branch)
-   # print("::set-output name=baseNm::" + main_repo)
-   # print("::set-output name=headNm::" + head_repo)
-   # print("::set-output name=pullNm::" + str(pull_number))
+
     
 if __name__ == "__main__":
     path = sys.argv[1]
